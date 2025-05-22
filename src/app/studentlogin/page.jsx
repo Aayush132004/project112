0499b35649c5d114675faba6cc146735b0659ce0
Aayush132004 <aayushsharma132004@gmail.com>
2025-05-22 21:45:50 +0530

    Update page.jsx

--- src/app/studentlogin/page.jsx
+++ src/app/studentlogin/page.jsx
@@ -23,7 +23,7 @@ export default function Login() {
     try {
       const res = await axios.post('/api/student/login', { aadhaarNumber, password });
       if (res.status === 200) {
-        console. log("login successful");
+        console.log("login successful");
       } else {
         alert(language === 'en' ? 'Invalid credentials. Please try again.' : 'अमान्य प्रमाण-पत्र। कृपया पुनः प्रयास करें।');
       }
